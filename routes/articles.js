78ccfb5cc6c19079b20472adc0e306abdb2256c7
Gemma <gemmawvoakes@gmail.com>
2017-06-23 18:37:26 +0100

    post comments by article code implemented.

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const models = require('../models/models');
+// const bodyParser = require('body-parser');
 
 router.get('/', function (req, res) {
     models.Articles.find()
@@ -21,7 +22,25 @@ router.get('/:articles_id/comments', function (req, res, next) {
         })
         .catch((err) => {
             if (err.name === 'CastError') {
-                return next({status: 422, message: 'Invalid Id'});
+                return next({ status: 422, message: 'Invalid Id' });
+            }
+            next(err);
+        });
+});
+router.post('/:articles_id/comments', function (req, res, next) {
+    // add comment to article
+    let newComment = new models.Comments({
+        body: req.body.body,
+        belongs_to: req.params.articles_id
+    });
+    newComment.save()
+        .then((newComment) => {
+            res.json(201, { comment: newComment });
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err.name === 'CastError') {
+                return next({ status: 422, message: 'Incorrect/Invalid ID' });
             }
             next(err);
         });
