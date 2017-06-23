ea8b3126b89aa99b5ec3ddc027e9969218841271
Joyfisher <joy.v.fisher@gmail.com>
2017-06-23 16:00:09 +0100

    commentsByArticleId implemented with working tests

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = 'test';
-const {expect} = require('chai');
+const { expect } = require('chai');
 const request = require('supertest');
 const server = require('../server');
 const saveTestData = require('../seed/test.seed');
@@ -9,10 +9,14 @@ const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 // console.log('******', db);
 
 describe('API', function () {
- // let usefulIds;
+  let usefulIds;
   before((done) => {
     mongoose.connection.dropDatabase()
-      .then(() => saveTestData(db, done));
+      .then(() => saveTestData(db, function (err, ids) {
+        if (err) throw err;
+        usefulIds = ids;
+        done();
+      }));
   });
   describe('GET /', function () {
     it('responds with status code 200', function (done) {
@@ -29,54 +33,79 @@ describe('API', function () {
   });
   describe('GET /topics', function () {
     it('responds with all topics', function (done) {
-     
+
       request(server)
         .get('/api/topics')
         .end((err, res) => {
           if (err) return console.log(err);
-            expect(res.status).to.equal(200);
-            expect(res.body.topics.length).to.equal(3);
-            done();
-  
+          expect(res.status).to.equal(200);
+          expect(res.body.topics.length).to.equal(3);
+          done();
+
         });
     });
   });
   describe('GET /api/topics/:topic_id/articles', function () {
     it('should return all of the articles that match the requested topic', function (done) {
-      
+
       request(server)
         .get('/api/topics/football/articles')
         .end((err, res) => {
           if (err) return console.log(err);
-            expect(res.status).to.equal(200);
-            expect(res.body.articlesByTopic.length).to.equal(1);
-            done();
+          expect(res.status).to.equal(200);
+          expect(res.body.articlesByTopic.length).to.equal(1);
+          done();
         });
     });
-    it('should return all of the articles that match the requested topic', function (done) {
-      
+    it('should return an error if the topic does not exist', function (done) {
+
       request(server)
         .get('/api/topics/coconuts/articles')
         .end((err, res) => {
           if (err) return console.log(err);
-            expect(res.status).to.equal(404);
-            // expect(res.body.articlesByTopic.length).to.equal(0);
-            //  console.log(res.body.articlesByTopic.length);
-            done();
+          expect(res.status).to.equal(404);
+          expect(res.body.message).to.equal('Topic not found');
+          done();
         });
     });
-});
- describe('GET /articles', function () {
+  });
+  describe('GET /articles', function () {
     it('responds with all articles', function (done) {
-     
+
       request(server)
         .get('/api/articles')
         .end((err, res) => {
           if (err) return console.log(err);
-            expect(res.status).to.equal(200);
-            expect(res.body.articles.length).to.equal(2);
-            done();
-  
+          expect(res.status).to.equal(200);
+          expect(res.body.articles.length).to.equal(2);
+          done();
+
+        });
+    });
+
+  });
+  describe('GET /api/articles/:article_id/comments', function () {
+    it('should return all of the comments that match the requested article', function (done) {
+      let idForArticle = usefulIds.article_id;
+      // console.log(idForArticle)
+      request(server)
+        .get(`/api/articles/${idForArticle}/comments`)
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.commentsForArticles.length).to.equal(2);
+          done();
+        });
+    });
+    it('should return an error if the article does not exist', function (done) {
+
+      request(server)
+        .get('/api/articles/594cf85ab75d862c4fbffa09/comments')
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(404);
+          expect(res.body.message).to.equal('Articles not found');
+          done();
         });
     });
   });
